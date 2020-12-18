94625b82178f313c84a8a561ebe0f7e409acfa4d
Tigran <levonyant1@gmail.com>
2020-12-18 20:32:14 +0400

    first version

---
+++ 1st_App/Calculator.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1st_App
+{
+    public class Calculator
+    {
+        public static double Calculate(string central)
+        {
+            central = central.Replace("-", "+-");
+
+            if (!central.Contains("("))
+            {
+                return SimpleOperators(central);
+            }
+
+            int i = central.IndexOf("(");
+            int i2 = central.Substring(i + 1).IndexOf("(") + i + 1;
+            int j2 = central.IndexOf(")");
+            if (j2 < i2)
+            {
+                string oper2 = central[j2 + 1].ToString();
+                string midcentral = central.Substring(i + 1, j2 - i - 1);  // (__)+()
+                string endcentral = central.Substring(j2 + 2); // ()+__(___)_
+                double midvalue = Calculate(midcentral);
+                double endvalue = Calculate(endcentral);
+
+                if (i != 0) // Example 1+(1+2)+(2+2)
+                {
+                    string oper1 = central[i - 1].ToString();
+                    string startcentral = central.Substring(0, i - 1);
+
+                    if (startcentral.Contains('+') && oper1 != "+")
+                    {
+                        string partofstart = startcentral.Substring(startcentral.IndexOf('+') + 1);
+                        midvalue = Separate(oper1, partofstart, midvalue);
+                        oper1 = "+";
+                        startcentral = startcentral.Substring(0, startcentral.IndexOf('+'));
+                    }
+
+                    double startvalue = SimpleOperators(startcentral);
+                    return SimpleOperators(startvalue + oper1 + midvalue + oper2 + endvalue);
+                }
+                else  // Example (1+2)+(2+2)
+                {
+                    return SimpleOperators(midvalue + oper2 + endvalue);
+                }
+            }
+            else
+            {
+                int j = central.LastIndexOf(")");
+
+                string midcentral = central.Substring(i + 1, j - i - 1); // (__)
+                double midvalue = Calculate(midcentral);
+
+                if (i != 0)
+                {
+                    if (j != central.Length - 1) // Example 1+(1+2)+2
+                    {
+                        string oper1 = central[i - 1].ToString();
+                        string oper2 = central[j + 1].ToString();
+                        string startcentral = central.Substring(0, i - 1);
+                        string endcentral = central.Substring(j + 2);
+                        if (startcentral.Contains('+') && oper1 != "+")
+                        {
+                            string partofstart = startcentral.Substring(startcentral.IndexOf('+') + 1);
+                            midvalue = Separate(oper1, partofstart, midvalue);
+                            oper1 = "+";
+                            startcentral = startcentral.Substring(0, startcentral.IndexOf('+'));
+                        }
+
+                        if (endcentral.Contains('+') && oper2 != "+")
+                        {
+                            string partofend = endcentral.Substring(0, endcentral.IndexOf('+'));
+                            midvalue = Separate(oper2, partofend, midvalue);
+                            oper2 = "+";
+                            endcentral = endcentral.Substring(endcentral.IndexOf('+') + 1);
+                        }
+
+                        double startvalue = SimpleOperators(startcentral);
+                        double endvalue = SimpleOperators(endcentral);
+                        return SimpleOperators(startvalue + oper1 + midvalue + oper2 + endvalue);
+                    }
+                    else // Example 1+(1+2)
+                    {
+                        string oper1 = central[i - 1].ToString();
+                        string startcentral = central.Substring(0, i - 1);
+                        if (startcentral.Contains('+') && oper1 != "+")
+                        {
+                            string partofstart = startcentral.Substring(startcentral.IndexOf('+') + 1);
+                            midvalue = Separate(oper1, partofstart, midvalue);
+                            oper1 = "+";
+                            startcentral = startcentral.Substring(0, startcentral.IndexOf('+'));
+                        }
+                        double startvalue = SimpleOperators(startcentral);
+                        return SimpleOperators(startvalue + oper1 + midvalue);
+                    }
+                }
+                else if (j != central.Length - 1) // Example (1+2)+2
+                {
+                    string oper2 = central[j + 1].ToString();
+                    string endcentral = central.Substring(j + 2);
+                    if (endcentral.Contains('+') && oper2 != "+")
+                    {
+                        string partofend = endcentral.Substring(0, endcentral.IndexOf('+'));
+                        midvalue = Separate(oper2, partofend, midvalue);
+                        oper2 = "+";
+                        endcentral = endcentral.Substring(endcentral.IndexOf('+') + 1);
+                    }
+                    double endvalue = SimpleOperators(endcentral);
+                    return SimpleOperators(midvalue + oper2 + endvalue);
+                }
+                else // Example (1+2)
+                {
+                    return SimpleOperators(midvalue.ToString());
+                }
+            }
+        }
+        private static double SimpleOperators(string startcentral)
+        {
+            double resultvalue = 0;
+            int Lengthstart = startcentral.Length;
+            int start = 0;
+            int end = startcentral.IndexOf('+');
+            string subcentral = "";
+
+            if (startcentral.Contains('+'))
+            {
+                for (int i = 0; i < Lengthstart; i++)
+                {
+                    subcentral = startcentral.Substring(start, end);
+                    if (!subcentral.Contains('*') && !subcentral.Contains('/') && !startcentral.Contains('^'))
+                    {
+                        resultvalue += Convert.ToDouble(subcentral);
+                        start = 0;
+                        if (end == startcentral.Length)
+                            break;
+                        startcentral = startcentral.Substring(end + 1);
+                        end = startcentral.Contains('+') ? startcentral.IndexOf('+') : startcentral.Length;
+                        continue;
+                    }
+                    resultvalue += ComplexOperators(subcentral);
+
+                    start = 0;
+                    if (end == startcentral.Length)
+                        break;
+                    startcentral = startcentral.Substring(end + 1);
+                    end = startcentral.Contains('+') ? startcentral.IndexOf('+') : startcentral.Length;
+
+                }
+            }
+            else if (startcentral.Contains('/') || startcentral.Contains('*') || startcentral.Contains('^'))
+            {
+                resultvalue = ComplexOperators(startcentral);
+            }
+            else
+            {
+                resultvalue = Convert.ToDouble(startcentral);
+            }
+            return resultvalue;
+        }
+
+        private static double ComplexOperators(string Subcentral)
+        {
+            double resultvalue1 = 0;
+            string num1 = "";
+            string num2 = "";
+            char op = ' ';
+            for (int j = 0; j < Subcentral.Length; j++)
+            {
+                if (Subcentral[j] == '*')
+                {
+                    if (op == '*')
+                    {
+                        num2 = (Convert.ToDouble(num2) * Convert.ToDouble(num1)).ToString();
+                        num1 = "";
+                    }
+                    else if (op == '/')
+                    {
+                        num2 = (Convert.ToDouble(num2) / Convert.ToDouble(num1)).ToString();
+                        num1 = "";
+                    }
+                    else if (op == '^')
+                    {
+                        num2 = (Math.Pow(Convert.ToDouble(num2), Convert.ToDouble(num1))).ToString();
+                        num1 = "";
+                    }
+                    else
+                    {
+                        num2 = num1;
+                        num1 = "";
+                    }
+                    op = '*';
+                }
+                else if (Subcentral[j] == '/')
+                {
+                    if (op == '*')
+                    {
+                        num2 = (Convert.ToDouble(num2) * Convert.ToDouble(num1)).ToString();
+                        num1 = "";
+                    }
+                    else if (op == '/')
+                    {
+                        num2 = (Convert.ToDouble(num2) / Convert.ToDouble(num1)).ToString();
+                        num1 = "";
+                    }
+                    else if (op == '^')
+                    {
+                        num2 = (Math.Pow(Convert.ToDouble(num2), Convert.ToDouble(num1))).ToString();
+                        num1 = "";
+                    }
+                    else
+                    {
+                        num2 = num1;
+                        num1 = "";
+                    }
+                    op = '/';
+                }
+                else if (Subcentral[j] == '^')
+                {
+                    if (op == '*')
+                    {
+                        num2 = (Convert.ToDouble(num2) * Convert.ToDouble(num1)).ToString();
+                        num1 = "";
+                    }
+                    else if (op == '/')
+                    {
+                        num2 = (Convert.ToDouble(num2) / Convert.ToDouble(num1)).ToString();
+                        num1 = "";
+                    }
+                    else if (op == '^')
+                    {
+                        num2 = (Math.Pow(Convert.ToDouble(num2), Convert.ToDouble(num1))).ToString();
+                        num1 = "";
+                    }
+                    else
+                    {
+                        num2 = num1;
+                        num1 = "";
+                    }
+                    op = '^';
+                }
+                else
+                {
+                    num1 += Subcentral[j];
+                }
+            }
+
+            if (op == '*')
+            {
+                resultvalue1 += Convert.ToDouble(num2) * Convert.ToDouble(num1);
+            }
+            else if (op == '/')
+            {
+                resultvalue1 += Convert.ToDouble(num2) / Convert.ToDouble(num1);
+            }
+            else if (op == '^')
+            {
+                resultvalue1 = Math.Pow(Convert.ToDouble(num2), Convert.ToDouble(num1));
+            }
+
+            return resultvalue1;
+        }
+        private static double Separate(string oper, string partstr, double midval) // 2+2*2*2*(1+2)
+        {
+            double partNum = 0;
+            if (partstr.Contains('*') || partstr.Contains('/') || partstr.Contains('^'))
+                partNum = ComplexOperators(partstr);
+            else
+                partNum = Convert.ToDouble(partstr);
+
+            if (oper == "*")
+                midval *= partNum;
+            else if (oper == "/")
+                midval /= partNum;
+            else if (oper == "^")
+                midval = Math.Pow(midval, partNum);
+
+            return midval;
+        }
+
+        public static double OperatorClickCalc(string text) // when click operator its calculate last all function or from open braces
+        {
+            string textcopy = text;
+            int OpInd = text.LastIndexOf('(');
+            for (; ; )
+            {
+                if (!textcopy.Substring(OpInd + 1).Contains(')'))
+                    break;
+                textcopy = textcopy.Remove(textcopy.Substring(OpInd + 1).IndexOf(')') + OpInd + 1, 1);
+                OpInd = textcopy.Substring(0, OpInd).LastIndexOf('(');
+            }
+            text = text.Substring(OpInd + 1);
+            double result = Calculate(text);
+            return result;
+        }
+        public static double CloseBracCalc(string text)
+        {
+            int count = 1;
+            int OpIndex = 0;
+            for (int i = text.Length - 1; i > 0; i--)
+            {
+                if (text[i - 1] == ')')
+                    count++;
+                else if (text[i - 1] == '(')
+                    count--;
+
+                if (count == 0)
+                {
+                    OpIndex = i - 1;
+                    break;
+                }
+            }
+            double result = Calculate(text.Substring(OpIndex));
+            return result;
+        }
+    }
+}
---
+++ 1st_App/MainWindow.xaml.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _1st_App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string allresult = "";
+        string result = "";
+        int bracksCount = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NumBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtDisplay2.Text.Contains('='))
+            {
+                allresult = "";
+                result = "";
+                txtDisplay.Text = "0";
+                txtDisplay2.Text = "0";
+            }
+
+            if (txtDisplay2.Text[txtDisplay2.Text.Length - 1] == ')')
+                return;
+
+            Button button = sender as Button;
+            result += button.Tag;
+            txtDisplay.Text = result;
+        }
+
+        //-------------------------------
+        // +- 1/x x2 sqrt Pi .
+
+        private void PointBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtDisplay2.Text[txtDisplay2.Text.Length - 1] == ')')
+                return;
+
+            if (result.Contains('.'))
+                return;
+
+            if (result == "")
+                result = txtDisplay.Text;
+
+            result += '.';
+            txtDisplay.Text = result;
+
+        }
+
+        private void BtnPi_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtDisplay2.Text[txtDisplay2.Text.Length - 1] == ')')
+                return;
+
+            result = Math.PI.ToString();
+            txtDisplay.Text = result;
+        }
+
+        private void BtnPosNeg_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtDisplay2.Text[txtDisplay2.Text.Length - 1] == ')')
+                return;
+
+            if (result == "")
+                result = txtDisplay.Text;
+
+            if (result[0] == '-')
+                result = result.Substring(1, result.Length - 1);
+            else
+                result = "-" + result;
+
+            txtDisplay.Text = result;
+        }
+
+        private void BtnRotate_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtDisplay2.Text[txtDisplay2.Text.Length - 1] == ')')
+                return;
+
+            if (result == "")
+                result = txtDisplay.Text;
+
+            result = (1 / Convert.ToDouble(result)).ToString();
+            txtDisplay.Text = result;
+        }
+
+        private void BtnSqrt_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtDisplay2.Text[txtDisplay2.Text.Length - 1] == ')')
+                return;
+
+            if (result == "")
+                result = txtDisplay.Text;
+
+            result = (Math.Sqrt(Convert.ToDouble(result))).ToString();
+            txtDisplay.Text = result;
+        }
+
+        private void BtnSquare_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtDisplay2.Text[txtDisplay2.Text.Length - 1] == ')')
+                return;
+
+            if (result == "")
+                result = txtDisplay.Text;
+
+            result = (Math.Pow(Convert.ToDouble(result), 2)).ToString();
+            txtDisplay.Text = result;
+        } 
+
+        // ------------------------------------
+        //    Brackets  ( )
+
+        private void BtnOpenBracket_Click(object sender, RoutedEventArgs e)
+        {
+            if (bracksCount >= 10)
+                return;
+
+            allresult += '(';
+            bracksCount++;
+            txtDisplay2.Text = allresult;
+        }
+
+        private void BtnCloseBracket_Click(object sender, RoutedEventArgs e)
+        {
+            if (bracksCount <= 0)
+                return;
+
+            if (result == "")
+                result = txtDisplay.Text;
+
+
+            allresult += result + ")";
+            bracksCount--;
+            result = "";
+            txtDisplay.Text = Calculator.CloseBracCalc(allresult).ToString();
+            txtDisplay2.Text = allresult;
+        }
+
+        //-------------------------------
+        //  Operators(+ - * / ^)    
+
+        private void OperatorsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtDisplay2.Text.Contains('='))
+            {
+                allresult = "";
+            }
+
+            if (txtDisplay2.Text[txtDisplay2.Text.Length - 1] != ')')
+            {
+                if (result == "")
+                    allresult += txtDisplay.Text;
+                else
+                    allresult += result;
+            }
+
+            if (bracksCount == 0)
+                txtDisplay.Text = Calculator.Calculate(allresult).ToString();
+            else
+                txtDisplay.Text = Calculator.OperatorClickCalc(allresult).ToString();
+
+
+            Button button = sender as Button;
+            allresult += button.Tag;
+            result = "";
+            txtDisplay2.Text = allresult;
+        }
+
+        //-------------------------------
+        // Clear buutons
+        private void BtnClearEntry_Click_1(object sender, RoutedEventArgs e)
+        {
+            result = "";
+            txtDisplay.Text = "0";
+        }
+
+        private void BtnClear_Click(object sender, RoutedEventArgs e)
+        {
+            result = "";
+            allresult = "";
+            txtDisplay.Text = "0";
+            txtDisplay2.Text = "0";
+        }
+
+        private void BtnBackspace_Click(object sender, RoutedEventArgs e)
+        {
+            if(result.Length > 0)
+                result = result.Substring(0, result.Length - 1);
+
+            if (result.Length > 0)
+                txtDisplay.Text = result;
+            else
+                txtDisplay.Text = "0";
+        }
+        //-------------------------------
+        private void BtnEquals_Click(object sender, RoutedEventArgs e)
+        {
+            allresult += result;
+            if (bracksCount > 0)
+            {
+                for (int i = 0; i < bracksCount; i++)
+                {
+                    allresult += ")";
+                }
+            }
+            txtDisplay2.Text = allresult + "=";
+            try
+            {
+                result = Calculator.Calculate(allresult).ToString();
+                txtDisplay.Text = result;
+            }
+            catch
+            {
+                MessageBox.Show("Your formula is invalid. Please try again");
+                txtDisplay.Text = "0";
+                txtDisplay2.Text = "0";
+                allresult = "";
+                result = "";
+            }
+        }
+
+        // Key properties
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.D6 && (Keyboard.Modifiers & ModifierKeys.Shift) == ModifierKeys.Shift)
+                btnSquareY.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+            else if (e.Key == Key.D9 && (Keyboard.Modifiers & ModifierKeys.Shift) == ModifierKeys.Shift)
+                btnOpenBracket.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+            else if (e.Key == Key.D0 && (Keyboard.Modifiers & ModifierKeys.Shift) == ModifierKeys.Shift)
+                btnCloseBracket.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+            else if (e.Key == Key.D0 || e.Key == Key.NumPad0)
+                btn0.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+            else if (e.Key == Key.D1 || e.Key == Key.NumPad1)
+                btn1.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+            else if (e.Key == Key.D2 || e.Key == Key.NumPad2)
+                btn2.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+            else if (e.Key == Key.D3 || e.Key == Key.NumPad3)
+                btn3.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+            else if (e.Key == Key.D4 || e.Key == Key.NumPad4)
+                btn4.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+            else if (e.Key == Key.D5 || e.Key == Key.NumPad5)
+                btn5.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+            else if (e.Key == Key.D6 || e.Key == Key.NumPad6)
+                btn6.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+            else if (e.Key == Key.D7 || e.Key == Key.NumPad7)
+                btn7.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+            else if (e.Key == Key.D8 || e.Key == Key.NumPad8)
+                btn8.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+            else if (e.Key == Key.D9 || e.Key == Key.NumPad9)
+                btn9.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+            else if (e.Key == Key.Add)
+                btnPlus.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+            else if (e.Key == Key.Multiply)
+                btnTimes.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+            else if (e.Key == Key.Subtract)
+                btnMinus.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+            else if (e.Key == Key.Divide)
+                btnDivide.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+            else if (e.Key == Key.OemPeriod || e.Key == Key.Decimal)
+                btnPoint.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+            else if (e.Key == Key.Enter)
+                btnEquals.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+            else if (e.Key == Key.Back)
+                btnBackspace.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+        }
+    }
+}
